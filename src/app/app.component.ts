b9b0eff1c5c6f9ca2c05cc81090426c5c546e6f2
Unknown <raphaelratuczne@gmail.com>
2019-04-29 13:14:27 -0300

    integrando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-angular7';
+
+  public campo = '';
+  public editando = null;
+
+  constructor(public dialog: MatDialog) { }
+
+  public lista = [
+    { texto: 'Teste de item', feito: false },
+    { texto: 'Essa lista não salva', feito: true },
+    { texto: 'Se apertar F5, já elvis...', feito: false }
+  ];
+
+  public salvar() {
+    if (this.editando !== null) {
+      this.lista[this.editando].texto = this.campo;
+      this.editando = null;
+    } else {
+      this.lista.push({ texto: this.campo , feito: false });
+    }
+    this.campo = '';
+  }
+
+  public editar(i:number) {
+    this.editando = i;
+    this.campo = this.lista[i].texto;
+  }
+
+  openDialog(index:number): void {
+    let dialogRef = this.dialog.open(DialogConfirmacao, {
+      width: '250px',
+      data: { i:index }
+    });
+
+    dialogRef.afterClosed().subscribe((result:number) => {
+      if(result >= 0) {
+        this.excluir(result);
+      }
+    });
+  }
+
+  public excluir(i:number) {
+    this.lista.splice(i,1);
+  }
+}
+
+
+@Component({
+  selector: 'dialog-confirmacao',
+  template: `
+    <h2 mat-dialog-title>Confirmar exclusão</h2>
+    <mat-dialog-content>Tem certeza que deseja ecluir?</mat-dialog-content>
+    <mat-dialog-actions>
+      <button mat-button [mat-dialog-close]="-1">Cancelar</button>
+      <button mat-button [mat-dialog-close]="data.i">Confirmar</button>
+    </mat-dialog-actions>
+  `
+})
+export class DialogConfirmacao {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogConfirmacao>,
+    @Inject(MAT_DIALOG_DATA) public data: any) {
+
+  }
+
+  fechar(i:number): void {
+    this.dialogRef.close(i);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -8,25 +9,30 @@ import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
 
-import { AppComponent } from './app.component';
+import { AppComponent, DialogConfirmacao } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogConfirmacao
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatButtonModule,
     MatToolbarModule,
     MatInputModule,
     MatCardModule,
     MatListModule,
     MatIconModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatDialogModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [DialogConfirmacao]
 })
 export class AppModule { }
